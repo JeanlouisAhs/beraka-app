b3cd5855ed97e23cb02f525e9183230a90ab1438
Jean-Louis <ahounansejeanlouis@gmail.com>
2023-09-19 09:45:46 +0100

    project init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'templates/**/*.html.twig',
+    'assets/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
